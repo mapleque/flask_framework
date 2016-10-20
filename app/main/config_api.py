ac4fe5a654dff47c78acb06ac17c7e355572d2bb
mapleque <mapleque@163.com>
2016-10-20 18:58:26 +0800

    config api

--- app/main/config_api.py
+++ app/main/config_api.py
@@ -1,5 +1,7 @@
+import json
 from . import main
+from flask import current_app
 
 @main.route('/config', methods=['GET', 'POST'])
 def config_api():
-  return 'config'
+  return json.dumps(current_app.config['SAMPLE'])
