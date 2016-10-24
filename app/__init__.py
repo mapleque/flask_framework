18baf0c18fdaa9be44105c3da0173c3c5e549a85
mapleque <mapleque@163.com>
2016-10-24 19:18:14 +0800

    logger handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from logging.handlers import RotatingFileHandler
 
 db = SQLAlchemy()
 
@@ -8,6 +9,8 @@ def create_app(config_name):
   app.config.from_pyfile(config_name)
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
+  handler = RotatingFileHandler('/tmp/flask.log')
+  app.logger.addHandler(handler)
   db.init_app(app)
 
   return app
--- app/main/simple_api.py
+++ app/main/simple_api.py
@@ -1,6 +1,7 @@
+from flask import current_app
 from . import main
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-  print 'here is log'
+  current_app.logger.error('here is log')
   return 'hello'
